3e75f7f8a7a6b05178a4be61776d8083782a00df
Sun_12138 <sun18250239663@gmail.com>
2023-11-29 11:48:26 +0800

    init

---
+++ src/test/java/com/group/study/GroupStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
